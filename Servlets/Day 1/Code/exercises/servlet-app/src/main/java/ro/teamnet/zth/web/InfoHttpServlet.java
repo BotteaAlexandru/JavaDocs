051a4c12693cc1f63ca6ea65e0d71635e4401663
BotteaAlexandru <alex.bottea@gmail.com>
2016-07-12 18:05:57 +0300

    servlets day 1 done

--- Servlets/Day 1/Code/exercises/servlet-app/src/main/java/ro/teamnet/zth/web/InfoHttpServlet.java
+++ Servlets/Day 1/Code/exercises/servlet-app/src/main/java/ro/teamnet/zth/web/InfoHttpServlet.java
@@ -1,11 +1,13 @@
 package ro.teamnet.zth.web;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
@@ -19,26 +21,56 @@ public class InfoHttpServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
         Map<String, String> map = new HashMap<String, String>();
-
+        Cookie[] arrayList = new Cookie[100];
+        String[] parameterValues = new String[200];
+        Enumeration parameterNames = req.getParameterNames();
         Enumeration headerNames = req.getHeaderNames();
+        PrintWriter out = resp.getWriter();
+        StringBuilder table = new StringBuilder();
+        StringBuilder cookieTable = new StringBuilder();
+        StringBuilder parameterTable = new StringBuilder();
+
         while (headerNames.hasMoreElements()) {
             String key = (String) headerNames.nextElement();
             String value = req.getHeader(key);
             map.put(key, value);
         }
-        StringBuilder table = new StringBuilder();
+
         table.append("<table> <tr>");
         for(String s : map.keySet()){
             table.append("<th>" + s + "</th>");
         }
-        table.append("</tr>");
+        table.append("</tr><tr>");
         for(String s : map.keySet()){
             table.append("<td>" + map.get(s) + "</td>");
         }
-        table.append("</table>");
-
-        PrintWriter out = resp.getWriter();
+        table.append("</tr></table>\n");
         out.write(table.toString());
+
+        out.write(req.getRequestURI() + "\n");
+        out.write(req.getQueryString() + "\n");
+
+        arrayList = req.getCookies();
+
+        cookieTable.append("<table><tr>");
+        for(Cookie cookie : arrayList){
+            cookieTable.append("<td>" + cookie.toString() + "</td>");
+        }
+        cookieTable.append("</tr></table>\n");
+        out.write(cookieTable.toString());
+
+        parameterTable.append("<table> <tr>");
+        while (parameterNames.hasMoreElements()){
+            parameterTable.append("<td>" + parameterNames.nextElement().toString() + "</td>");
+        }
+        parameterTable.append("</tr><tr>");
+        for(String s : parameterValues){
+            parameterTable.append("<td>" + s + "</td>");
+        }
+        parameterTable.append("</tr></table>\n");
+
+        out.write(parameterTable.toString());
+
 
 
     }
